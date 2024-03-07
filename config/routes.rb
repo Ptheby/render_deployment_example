11edc09dea64f4b8c841ce8ad0c0ad723536439a
Paul Theby <paultheby@gmail.com>
2024-03-07 13:48:37 -0600

    image attachd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create]
+
+  resources :users do
+    post 'upload_image', to: 'users#upload_image'
+  end
+
   # root "posts#index"
 end
